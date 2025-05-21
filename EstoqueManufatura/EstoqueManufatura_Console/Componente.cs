3ed7a4f7ccdf018e115c6758f35d54ea24eb35c3
l.bernardes@pg.inatel.br <l.bernardes@pg.inatel.br>
2025-05-21 19:44:14 -0300

    Aula 1.2
    Adiciona novos projetos e classes para gerenciamento de componentes
    
    Atualiza a solução `EstoqueManufatura.sln` para incluir os projetos `EstoqueManufatua_API` e `EstoqueManufatura_Console`, removendo o `WebApplication1.csproj`.
    
    Adiciona a classe `Componente` para gerenciar componentes e suas plataformas, e a classe `Projeto` para representar plataformas.
    
    Cria o arquivo `Program.cs` com a lógica principal do aplicativo de console, permitindo registrar e exibir componentes e plataformas.
    
    Configura os novos projetos para o framework `net8.0` com usings implícitos e nullable.

---
+++ EstoqueManufatura/EstoqueManufatura_Console/Componente.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueManufatura_Console
+{
+    internal class Componente
+    {
+        public Componente(string pn, string descricao)
+        {
+            PN = pn;
+            Descricao = descricao;
+        }
+
+        public string PN { get; set; } // Part Number
+                                        
+        public string Descricao { get; set; } // Description
+
+        private List<Projeto> plataformas = new();
+        public override string ToString()
+        {
+            return $"PN: {PN}";
+        }
+
+        public void AdicionarProjeto(Projeto p)
+        {
+            plataformas.Add(p);
+        }
+
+        public void ShowPlataformas()
+        {
+            Console.WriteLine($"Plataformas do componente {PN}");
+            if (plataformas.Count == 0)
+            {
+                Console.WriteLine("Nenhuma plataforma registrada.");
+                return;
+            }
+            else
+            {
+                foreach (Projeto p in plataformas)
+                {
+                    Console.WriteLine(p);
+                }
+            }
+        }
+    }
+}
---
+++ EstoqueManufatura/EstoqueManufatura_Console/Projeto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueManufatura_Console
+{
+    internal class Projeto
+    {
+        public string Plataforma { get; set; } // Project name
+
+        public string Montadora { get; set; } // Project description
+
+        public Projeto(string plataforma, string montadora)
+        {
+            Plataforma = plataforma;
+            Montadora = montadora;
+        }
+
+        public override string ToString()
+        {
+            return $"Plataformas do componente: {Plataforma} \n";
+        }
+    }
+}
