285a292ee82e677d812372031212c9e561547171
l.bernardes@pg.inatel.br <l.bernardes@pg.inatel.br>
2025-06-01 16:15:40 -0300

    2.3

---
+++ EstoqueManufatura/EstoqueManufatura.Shared.Data/BD/Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EstoqueManufatura_Console;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
+
+namespace EstoqueManufatura.Shared.Data.BD
+{
+    public class Context : DbContext
+    {
+
+        public DbSet<Componente> Componente { get; set; }
+        public DbSet<Projeto> Projeto { get; set; }
+
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EstoqueManufatura_BD;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
